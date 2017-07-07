a8d6e5a3485f4328144d9f903f4405e81b338347
cdelsner <cdelsner@email.wm.edu>
2017-07-07 16:33:35 -0400

    change

--- scripts/script_5.R
+++ scripts/script_5.R
@@ -2,7 +2,6 @@ rm(list=ls())
 setwd("~/GitHub/Liberia")
 
 
-
 library(rgdal)
 library(dplyr)
 library(rgeos)
@@ -71,31 +70,104 @@ lbr_dist_f<-fortify(lbr_dist)
 lbr_clan_f<-fortify(lbr_clan)
 
 
+
 ## Import WorldPop ##
-wp_lbr<-raster("~/Documents/wm/year1/monroe_project/LBR-POP/LBR10adjv3.tif")
- 
+wp_lbr<-raster("~/GoogleDrive/LiberiaProject/LBR-WP/LBR10adjv3.tif")
+proj4string(wp_lbr)
 
 ## Import GPW4 data ##
 gpw4_2010<-raster("~/GoogleDrive/LiberiaProject/gpw-v4-population-count_2010.tif")
 gpw4_2010<-crop(gpw4_2010,extent(lbr))
 gpw4_2010<-mask(gpw4_2010,lbr)
 
+## IMPORT OSM DATA ##
+dsn<-"/Users/chriselsner/GoogleDrive/LiberiaProject/LBR-OSM"
+bldgs<-readOGR(dsn = "/Users/chriselsner/GoogleDrive/LiberiaProject/LBR-OSM", layer = "gis.osm_buildings_a_free_1", stringsAsFactors=FALSE, verbose=FALSE)
+proj4string(bldgs)
+
 
 ## Load final data ##
-load("lbr_gpw4_data.RData")
+load("RData/lbr_gpw4_data.RData")
 
 
 ## Load pixel boundaries data frame ##
-load("lbr_gpw4_pixels.RData")
+load("RData/lbr_gpw4_pixels.RData")
 
 
 ## complete pop and spatial data set ##
-load("lbr_gpw4_complete.RData")
-
-
-## load population samples ##
-load("points_df_10percent.RData")
+load("RData/lbr_gpw4_complete.RData")
 
 
 ## load city points data ##
-load("city_points.RData")
+load("RData/city_points.RData")
+
+
+# TESTING STUFF -----
+
+quartz()
+blueblue<-colorRampPalette(brewer.pal(9,"Blues"))(250)
+
+bain<-lbr_clan[which(lbr_clan@data$CLNAME=="Bain"),]
+plot(bain)
+wp_bain<-crop(wp_lbr,bain)
+wp_bain<-mask(wp_bain, bain)
+
+pop<-sum(wp_bain@data@values,na.rm = T)
+
+wp_bain_im <- as.im(wp_bain)
+plot(wp_bain_im)
+class(wp_bain_im)
+class(wp_lbr)
+
+bain_win<- as(bain,"owin")
+bain_ppp <- rpoint(pop, f=wp_bain_im,bain_win)
+bain_ppp_im<-as.im(bain_ppp)
+plot(bain_ppp_im)
+bain_dens<-density(bain_ppp,sigma=bw.ppl)
+plot(bain_dens)
+summary(bain_ppp_im)
+summary(bain_dens)
+
+bain_cont<-contour(wp_bain_im,add=T)
+
+# Clustering Funnction -----
+contourclust<-function(pobj,){ # pobj = point pobject (raster, im, ppp)
+  if (class(pobj) == "raster"){
+    pobj<-as.im(pobj)
+  }else if (class(pobj) == "im"){
+    
+  }
+}
+Dsg <- as(wp_bain_im, "SpatialGridDataFrame")  # convert to spatial grid class
+Dim <- as.image.SpatialGridDataFrame(Dsg)  # convert again to an image
+Dcl <- contourLines(Dim, nlevels = 100)  # create contour object - change 8 for more/fewer levels
+SLDF <- ContourLines2SLDF(Dcl) # convert to SpatialLinesDataFrame
+plot(SLDF, col = terrain.colors(100))
+as(SpatialPoints(SLDF), "ppp")
+xxx <- SLDF[-c(1,3,4), ]
+class(xxx@lines[[1]])
+class(xxx)
+Polyclust <- gPolygonize(xxx)
+plot(Polyclust)
+gas <- gArea(Polyclust, byid = T)
+Polyclust <- SpatialPolygonsDataFrame(Polyclust, data = data.frame(gas), match.ID = F)
+plot(Polyclust)
+# Workaround 
+pxy <- cbind(p$x,p$y)
+pxy <- as.data.frame(pxy)
+
+pxy$observation <- 1:nrow(pxy) 
+pxy$observation <- as.data.frame(pxy$observation)
+
+dfxy <- as.data.frame(cbind(pxy$V1,pxy$V2))
+
+chocho <- SpatialPointsDataFrame(dfxy,pxy$observation)
+cAg <- aggregate(chocho, by = Polyclust, FUN = length)
+
+
+#Add cex= to change point size 
+plot(p, main = "Pixel Image with Function based on Density of Raster Map",cex=.03)
+rp<-raster(as.im(p))
+open3d()
+plot3D(rp, col = blueblue)
+
